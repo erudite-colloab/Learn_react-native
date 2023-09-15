8a8c056a9831bff70db7910635fd5dc0982650d6
erudite-colloab <eugenebaahasamoah@gmail.com>
2023-09-15 09:02:07 +0000

    Started

---
+++ StickerSmash/App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, } from 'react-native';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+
+const PlaceHolderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource ={PlaceHolderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#575761',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
